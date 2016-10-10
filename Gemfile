0bb4b768f762f527b83581c2330ef3504ad2d622
nhatan172 <nhatan172@gmail.com>
2016-10-10 23:49:25 +0700

    Make a basic User model (including secure passwords

--- Gemfile
+++ Gemfile
@@ -45,6 +45,8 @@ group :development do
   gem 'spring-watcher-listen', '~> 2.0.0'
   gem 'rspec-rails'
   gem 'pg'
+  gem 'annotate'
+  gem 'bcrypt'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  resources :users
+  	get 'users/new'
 
-  get 'static_pages/help'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	root to: 'static_pages#home'
+	
+	match "/static_pages/home", to: 'static_pages#home' , via: :get
+	match '/help', to: 'static_pages#help', via: :get
+	match '/about', to: 'static_pages#about', via: :get
+	match '/contact', to: 'static_pages#contact', via: :get
+	match '/signup', to: 'users#new' , via: :get
 end
